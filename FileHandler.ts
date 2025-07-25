f9c64a66d32aa129ffad8b06ac571c939cf605b2
vorotamoroz <vrtmrz@gmail.com>
2025-07-25 05:20:15 +0100

    And fix again.

--- FileHandler.ts
+++ FileHandler.ts
@@ -1,4 +1,4 @@
-import { serialized } from "octagonal-wheels/concurrency/lock";
+import { serialized } from "npm:octagonal-wheels/concurrency/lock";
 import { ensureDir } from "./paths.ts";
 
 let teeStdError: boolean = false;
